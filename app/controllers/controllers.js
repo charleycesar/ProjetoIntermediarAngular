89c571e239a616e82bbfd1db4d4f94e4c1299eda
cry <cry@cry-Master.(none)>
2014-11-05 00:18:27 -0200

    finalizando galeria

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -73,35 +73,24 @@ app.controller('EquipeCtrl', function($rootScope, $location){
 
 app.controller('GaleriaCtrl', function($rootScope, $scope, $location){
     path = '/onpage/app/images/galeria/'
-     $scope.colors = ["#fc0003", "#f70008", "#f2000d", "#ed0012", "#e80017", "#e3001c", "#de0021", "#d90026", "#d4002b", "#cf0030", "#c90036", "#c4003b", "#bf0040", "#ba0045", "#b5004a", "#b0004f", "#ab0054", "#a60059", "#a1005e", "#9c0063", "#960069", "#91006e", "#8c0073", "#870078", "#82007d", "#7d0082", "#780087", "#73008c", "#6e0091", "#690096", "#63009c", "#5e00a1", "#5900a6", "#5400ab", "#4f00b0", "#4a00b5", "#4500ba", "#4000bf", "#3b00c4", "#3600c9", "#3000cf", "#2b00d4", "#2600d9", "#2100de", "#1c00e3", "#1700e8", "#1200ed", "#0d00f2", "#0800f7", "#0300fc"];
-
-    function addSlide(target, style) {
-        var i = target.length;
-        target.push({
-            id: (i + 1),
-            label: 'slide #' + (i + 1),
-            img: 'http://lorempixel.com/450/300/' + style + '/' + ((i + 1) % 10) ,
-            color: $scope.colors[ (i*10) % $scope.colors.length],
-            odd: (i % 2 === 0)
-        });
-    };
-
-    $scope.carouselIndex = 3;
-    $scope.carouselIndex2 = 0;
-    $scope.carouselIndex2 = 1;
-    $scope.carouselIndex3 = 5;
-    $scope.carouselIndex4 = 5;
-
-    function addSlides(target, style, qty) {
-        for (var i=0; i < qty; i++) {
-            addSlide(target, style);
-        }
+    $scope.Images = [{
+      label:'Mônica Farias',
+      'descricao':'Psicóloga, mestre em Psicologia pela UFF, especialista em Psicomotricidade pelo Anthropos.<br> Mediadora escolar, acompanhante terapêuta (Esquizoanalise).<br> Orientadora Educacional seguimento Educação Infantil e Creche ISERJ .',
+      img:path+'IMG_8765.JPG'
+    },{
+      'nome':'Kamilla Cardoso',
+      'descricao':'pedagoga, especialista em Alfabetização, Leitura e Escrita pela UFRJ, Técnica de Assuntos Educacionais do Colégio Pedro Ii, atuando como orientadora Educacional. Já atuou como professora do primeiro.',
+      img:path+'IMG_8767.JPG'
+    },{
+      'nome':'Mariana Barbosa de Castro',
+      'descricao':'psicóloga pela UFF, atualmente (Junho/2013) fazendo mediação, no Colégio Coração Eucarístico- Flamengo, criança com diagnostico de melingocele.',
+      img:path+'IMG_8775.JPG'
+    },{
+      'nome':'Mariana Barbosa de Castro',
+      'descricao':'psicóloga pela UFF, atualmente (Junho/2013) fazendo mediação, no Colégio Coração Eucarístico- Flamengo, criança com diagnostico de melingocele.',
+      img:path+'IMG_8824.JPG'
     }
-
-    // 1st ngRepeat demo
-    $scope.slides = [];
-    addSlides($scope.slides, 'sports', 50);
-
+    ];
     $rootScope.activetab = $location.path();
 });
  
